2cb4714816c782313dbdfe622d027a372585adcb
Zeerak Waseem <zeerak.w@gmail.com>
2017-04-20 19:27:30 +0100

    Add splits and split expert annotated data into annotation based lists

--- dataset_reader.py
+++ dataset_reader.py
@@ -14,17 +14,37 @@ class DataSet():
 
 
 if __name__=="__main__":
-    racism = DataSet("Racism")
-    print(len(racism.data))
+    racism = DataSet("racism_overlapfree")
+    splits = int(len(racism.data) * 0.8), int(len(racism.data) * 0.9)
+    train, dev, test = racism.data[:splits[0]], racism.data[splits[0]:splits[1]], racism.data[splits[1]:]
+    print(len(racism.data), len(train), len(dev), len(test))
 
-    for tweet in racism.data:
-        print(tweet['text'])
+    # for tweet in racism.data:
+    #     print(tweet['text'])
+
+    neither = DataSet("neither_overlapfree")
+    splits = int(len(neither.data) * 0.8), int(len(neither.data) * 0.9)
+    train, dev, test = neither.data[:splits[0]], neither.data[splits[0]:splits[1]], neither.data[splits[1]:]
+    print(len(neither.data), len(train), len(dev), len(test))
 
 
-    neither = DataSet("neither")
-    print(len(neither.data))
+    sexism = DataSet("sexism_overlapfree")
+    splits = int(len(sexism.data) * 0.8), int(len(sexism.data) * 0.9), int(len(sexism.data) * 0.1)
+    train, dev, test = sexism.data[:splits[0]], sexism.data[splits[0]:splits[1]], sexism.data[splits[1]:]
+    print(len(sexism.data), len(train), len(dev), len(test))
 
+    # Read Expert annotations
+    expert = DataSet('amateur_expert')
+    e_racism, e_sexism, e_neither, e_both = [], [], [], []
 
-    sexism = DataSet("sexism")
-    print(len(sexism.data))
-
+    for item in expert.data:
+        if item['Annotation'].lower() == 'sexism':
+            e_sexism.append(item)
+        elif item['Annotation'].lower() == 'racism':
+            e_racism.append(item)
+        elif item['Annotation'].lower() == 'neither':
+            e_neither.append(item)
+        else:
+            # TODO Consider merging this into 'racism' to boost counts there
+            e_both.append(item)
+    print(len(e_racism), len(e_neither), len(e_sexism), len(e_both))
